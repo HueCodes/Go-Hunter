75a7489dd9038c51a6517ed906bc16d1453c2eac
Hugh <Hugh.t.Ryan@gmail.com>
2025-12-21 16:43:56 -0800

    Add structured logging with slog
    Support text output for development and JSON for production.

---
+++ pkg/util/logger.go
@@ -0,0 +1,23 @@
+package util
+
+import (
+	"log/slog"
+	"os"
+)
+
+func NewLogger(env string) *slog.Logger {
+	var handler slog.Handler
+
+	opts := &slog.HandlerOptions{
+		Level: slog.LevelInfo,
+	}
+
+	if env == "development" {
+		opts.Level = slog.LevelDebug
+		handler = slog.NewTextHandler(os.Stdout, opts)
+	} else {
+		handler = slog.NewJSONHandler(os.Stdout, opts)
+	}
+
+	return slog.New(handler)
+}
