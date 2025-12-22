8eb03366219b2c46a6cc20fa5ea3acde15fdaff2
Hugh <Hugh.t.Ryan@gmail.com>
2025-12-21 16:44:07 -0800

    Add base model with UUID primary key
    Define common fields for all database models.

---
+++ internal/database/models/base.go
@@ -0,0 +1,23 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+// Base model with UUID primary key and timestamps
+type Base struct {
+	ID        uuid.UUID      `gorm:"type:uuid;primary_key" json:"id"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
+}
+
+func (b *Base) BeforeCreate(tx *gorm.DB) error {
+	if b.ID == uuid.Nil {
+		b.ID = uuid.New()
+	}
+	return nil
+}
