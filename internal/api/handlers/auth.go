df6abee6af98612d289bf615ae3175a180b10edc
Hugh <Hugh.t.Ryan@gmail.com>
2026-01-06 17:01:40 -0800

    Fix error message formatting in auth handler

--- internal/api/handlers/auth.go
+++ internal/api/handlers/auth.go
@@ -126,5 +126,5 @@ func (h *AuthHandler) Logout(w http.ResponseWriter, r *http.Request) {
 func writeJSON(w http.ResponseWriter, status int, v interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
-	json.NewEncoder(w).Encode(v)
+	_ = json.NewEncoder(w).Encode(v)
 }
